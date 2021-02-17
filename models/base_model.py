878d227da33d18b304aa8b80c73660b2f27ba43e
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-02-16 23:24:58 -0500

    First version

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,31 @@
 #!/usr/bin/python3
-import uuid
-import datetime
+from datetime import datetime
 import models
+import uuid
 
 class BaseModel:
+    """
+    a class that defines all common attributes/methods for other classes
+    """
     def __init__(self, *args, **kwargs):
         if kwargs is None or len(kwargs) < 1:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
         else:
             for key in kwargs:
                 if key != "__class__":
                     setattr(self, key, kwargs[key])
-            self.created_at = datetime.datetime.fromisoformat(kwargs["created_at"])
-            self.updated_at = datetime.datetime.fromisoformat(kwargs["updated_at"])
+            self.created_at = datetime.fromisoformat(kwargs["created_at"])
+            self.updated_at = datetime.fromisoformat(kwargs["updated_at"])
  
     def __str__(self,):
         x = "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
         return x
 
     def save(self):
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
         models.storage.save()
 
 
